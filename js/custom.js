d398415f26eac651727e1e78478e9f7766fc3a45
siam4845 <siamtalukder4845@gmail.com>
2021-08-24 03:35:15 +0600

    siam

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+
+
+//===========testimonial part slider===========
+$('.testi-item-wrapper').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows:true,
+    prevArrow:'.testi-prev',
+    nextArrow:'.testi-next',
+    speed:2000,
+  });
