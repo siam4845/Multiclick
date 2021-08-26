085940f0ec92fa4d7c5d49f26e56c70fabee2d53
siam4845 <siamtalukder4845@gmail.com>
2021-08-27 02:38:53 +0600

    hi

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,3 @@
-
-
 //===========testimonial part slider===========
 $('.testi-item-wrapper').slick({
     infinite: true,
@@ -10,3 +8,29 @@ $('.testi-item-wrapper').slick({
     nextArrow:'.testi-next',
     speed:2000,
   });
+
+  //============video part start============
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+function toggle(button) {
+  if (video.paused) {
+    video.play();
+    document.getElementById('myBtn').innerHTML = '<i class="fa fa-pause"></i>';
+    document.getElementById('siam-player').innerHTML = 'Pause Video';
+  } 
+  else {
+    video.pause();
+    document.getElementById('myBtn').innerHTML = '<i class="fa fa-play"></i>';
+    document.getElementById('siam-player').innerHTML = 'Play Video';
+  }
+}
+
+
+
+//====================counter part======================
+$(function(){
+  $('.counter').counterUp({
+    delay: 5,
+    time: 3000,
+  });
+});
